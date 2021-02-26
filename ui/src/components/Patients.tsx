3d3af99a68e5da0ec93c89f87f5aea6e95ae25b8
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2021-02-26 15:58:26 +0000

    Factor out common components/utils

--- ui/src/components/Patients.tsx
+++ ui/src/components/Patients.tsx
@@ -3,23 +3,14 @@ import { Link, NavLink, Redirect, Route, Switch, useRouteMatch, useParams } from
 import { Main } from '@daml.js/healthcare-claims-processing';
 import { useStreamQuery } from '@daml/react';
 import { CaretRight, Share } from "phosphor-react";
+import { innerJoin, intercalate, Field, FieldsRow, PageTitle, TabLink } from "./Common";
 
-type Map<V> = { [key: string]: V };
-type FieldProps = {label: string, value: string};
 
 type PatientOverview =
   { acceptance: Main.Patient.NotifyPatientOfPCPAcceptance,
     policy: Main.Policy.DisclosedPolicy,
   };
 
-function intercalate<X>(xs: X[], sep: X) {
-  return xs.flatMap(x => [sep,x]).slice(1);
-}
-
-function innerJoin<X,Y>(xs: Map<X>, ys: Map<Y>): Map<[X,Y]> {
-  const keys = Object.keys(xs).filter(k => ys[k] != undefined);
-  return Object.fromEntries(keys.map(k => [k, [xs[k], ys[k]]]));
-}
 
 const PatientRoutes: React.FC = () => {
   const match = useRouteMatch();
@@ -158,52 +149,6 @@ const Patient: React.FC = () => {
 
       </div>
     </>
-  )
-}
-
-const TabLink: React.FC<{to:string}> = ({children,to}) => {
-  const match = useRouteMatch();
-  return (
-    <NavLink
-      exact
-      to={to}
-      className="text-sm px-2 py-1 text-blue"
-      activeStyle={{
-        background: "white",
-        color: "black",
-      }}
-    >
-      {children}
-    </NavLink>
-  )
-}
-
-const PageTitle: React.FC<{title:string}> = ({title}) => {
-  return (
-    <div className="text-3xl text-trueGray-700"> {title} </div>
-  )
-}
-
-const FieldsRow: React.FC<{fields: FieldProps[]}> = ({fields}) => {
-  return (
-    <div className="flex space-x-12">
-      { fields.map(f =>
-        <Field label={f.label} value={f.value} />
-      )}
-    </div>
-  )
-}
-
-const Field: React.FC<FieldProps> = ({label, value}) => {
-  return (
-    <div className="flex flex-col">
-      <div className="text-sm text-trueGray-500">
-        {label}
-      </div>
-      <div className="text-base">
-        {value}
-      </div>
-    </div>
   )
 }
 
