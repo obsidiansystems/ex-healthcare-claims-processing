fcc8ad6dcfeddfa2a14fe6ef2f2e8bb45d930bfb
John Ericson <John.Ericson@Obsidian.Systems>
2021-04-06 12:30:33 -0400

    Make nix-shell work with cleared nix path

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import ./nix/deps/nixpkgs {}, daml ? import ./nix/deps/daml.nix {} }:
+{ pkgs ? import ./nix/deps/nixpkgs {}, daml ? import ./nix/deps/daml.nix { inherit pkgs; } }:
 pkgs.mkShell {
   name = "daml-shell";
   nativeBuildInputs = [ daml pkgs.openjdk pkgs.nodejs-15_x ];
