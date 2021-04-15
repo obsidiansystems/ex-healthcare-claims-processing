db3c4d315243b3cf8599a22bf5ed72b2b5136c31
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2021-04-16 00:54:04 +0100

    Filter tabs by role

--- ui/src/components/MainScreen.tsx
+++ ui/src/components/MainScreen.tsx
@@ -13,42 +13,88 @@ import { useParty } from '@daml/react';
 import { Link, Route } from 'react-router-dom';
 import { TabularScreenRoutes } from './TabularScreen';
 import '@fontsource/alata';
-// import * as phos from 'phosphor-react';
 
 type Props = {
   onLogout: () => void;
 }
 
-const TabLink : React.FC<{to: string, exact?: boolean, icon: string}> = ({to, children, icon, exact}) => {
+type TabProps = {to: string, exact?: boolean, icon: string, label: string}
+
+const TabLink : React.FC<TabProps> = ({to, label, icon, exact}) => {
   return <Route path={to} exact={exact} children={({ match }) => (
-    <Link to={to} className={"flex flex-grow-0 h-9 items-center text-blue text-sm font-alata mr-3 ml-3 mt-1 mb-1 rounded" + (match ? " tab-active" : " tab-hover") }><i className={"ph-"+icon+" text-blueGray-400 text-2xl center m-4"}/>{children}</Link>
-    )} />;
+    <Link to={to} className={"flex flex-grow-0 h-9 items-center text-blue text-sm font-alata mr-3 ml-3 mt-1 mb-1 rounded" + (match ? " tab-active" : " tab-hover") }><i className={"ph-"+icon+" text-blueGray-400 text-2xl center m-4"}/>{label}</Link>
+  )} />;
 };
 
-/**
- * React component for the main screen of the `App`.
- */
+const tabs = {
+  profile: {
+    to: "/",
+    icon: "user",
+    exact: true,
+    label: "Profile",
+  },
+  referrals: {
+    to: "/provider/referrals",
+    icon: "tray",
+    label: "Referrals",
+  },
+  appointments: {
+    to: "/provider/appointments",
+    icon: "calendar-blank",
+    label: "Appointments",
+  },
+  treatments: {
+    to: "/provider/treatments",
+    icon: "first-aid-kit",
+    label: "Treatments",
+  },
+  claims: {
+    to: "/provider/claims",
+    icon: "currency-circle-dollar",
+    label: "Claims",
+  },
+  patients: {
+    to: "/provider/patients",
+    icon: "pedestrian",
+    label: "Patients",
+  },
+  bills: {
+    to: "/patient/bills",
+    icon: "currency-circle-dollar",
+    label: "Bills",
+  },
+};
+
+const sidebar: Map<string, Array<TabProps>> = new Map([
+  ["Patient 1",
+   [tabs.profile, tabs.appointments, tabs.treatments, tabs.bills]],
+  ["PrimaryCareProvider",
+   [tabs.profile, tabs.referrals, tabs.patients]],
+  ["Radiologist",
+   [tabs.profile, tabs.referrals, tabs.appointments, tabs.treatments, tabs.claims, tabs.patients]],
+  ["Insurance Company",
+   [tabs.claims]],
+])
+
 const MainScreen: React.FC<Props> = ({onLogout}) => {
   const [modalActive,setModalActive] = React.useState(false);
   const [date, setDate] = React.useState(new Date());
   const formatDate = (d:Date) => dateFormat(d, "ddd, mmm d, yyyy");
-  const role = useParty(); // TODO: proper role handling
+  const role = useParty();
   const theme = {
     blue: '#4c6fea',
   }
+
+  const roleTabs = sidebar.get(role) ?? [];
 
   return (
     <div className="main-grid font-alata">
       <div className="bg-trueGray-50 flex flex-col justify-start text-sm text-trueGray-500"> {/*px-20 inset-y-0 bg-blue w-64 object-center">*/}
         <img src="/logo-with-name.svg" className="inline-block ml-px30 mt-px25 mb-7 self-start"/>
-        <TabLink icon="user" to="/" exact={true}>Profile</TabLink>
-        <TabLink icon="tray" to="/provider/referrals">Referrals</TabLink>
-        <TabLink icon="calendar-blank" to="/provider/appointments">Appointments</TabLink>
-        <TabLink icon="first-aid-kit" to="/provider/treatments">Treatments</TabLink>
-        <TabLink icon="currency-circle-dollar" to="/provider/claims">Claims</TabLink>
-        <TabLink icon="pedestrian" to="/provider/patients">Patients</TabLink>
-        {/*<TabLink icon="handshake" to="/provider/payers">Insurance Providers</TabLink>*/}
-        <TabLink icon="currency-circle-dollar" to="/patient/bills">Bills</TabLink>
+
+        { roleTabs.map(({to, exact, icon, label}) =>
+          <TabLink icon={icon} to={to} exact={exact} label={label}/>
+          )}
         <div className="flex-grow"/>
         <hr className="mx-3"/>
         <div className="mx-7 py-2">
@@ -74,15 +120,15 @@ const MainScreen: React.FC<Props> = ({onLogout}) => {
         <Formik initialValues={{newDate: Nothing }} onSubmit={({}) => {} /* change time itself */}>
           {({ errors, touched, isValidating, isSubmitting}) => (
             <Form>
-            <DayPickerField name="newDate" />
-            <div className="flex justify-center align-center">
-              <SubmitButton submitTitle={"Set Date"} isSubmitting={isSubmitting} />
-            </div>
-            {/* <Field name="email" validate={validateEmail} />
-                {errors.email && touched.email && <div>{errors.email}</div>}
-                <Field name="username" validate={validateUsername} />
-                {errors.username && touched.username && <div>{errors.username}</div>}
-                <button type="submit">Submit</button> */}
+              <DayPickerField name="newDate" />
+              <div className="flex justify-center align-center">
+                <SubmitButton submitTitle={"Set Date"} isSubmitting={isSubmitting} />
+              </div>
+              {/* <Field name="email" validate={validateEmail} />
+                  {errors.email && touched.email && <div>{errors.email}</div>}
+                  <Field name="username" validate={validateUsername} />
+                  {errors.username && touched.username && <div>{errors.username}</div>}
+                  <button type="submit">Submit</button> */}
             </Form>)}
         </Formik>
       </Modal>
