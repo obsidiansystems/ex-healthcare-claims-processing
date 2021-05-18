obsidiansystems/ex-healthcare-claims-processing

commit 1877df70460d28ec1933656d3f599a67a3142627

Only referred provider can schedule appointment

ui/src/components/Referrals.tsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from 'react'
 import { Link, NavLink, Redirect, Route, Switch, useRouteMatch, useParams } from 'react-router-dom';
 import { Main } from '@daml.js/healthcare-claims-processing';
 import { CreateEvent } from '@daml/ledger';
-import { useStreamQuery, useLedger } from '@daml/react';
+import { useStreamQuery, useLedger, useParty } from '@daml/react';
 import { CaretRight, Share } from "phosphor-react";
 import { mapIter, innerJoin, intercalate, Field, FieldsRow, Message, TabLink, useAsync } from "./Common";
 import { Formik, Form, Field as FField, useField } from 'formik';
@@ -53,6 +53,7 @@ const useReferralData = () => {
 }
 
 const Referral: React.FC = () => {
+  const role = useParty();
   return <SingleItemView
     title="Referral"
     useData={useReferralData}
@@ -64,7 +65,7 @@ const Referral: React.FC = () => {
     ]] }
     tableKey={ o => o.overview.referral.contractId }
     itemUrl={ o => "" }
-    choices={ d => [
+    choices={ d => d?.overview?.referral?.payload?.renderingProvider == role ? [
             <ChoiceModal className="flex flex-col"
                          choice={Main.Provider.ReferralDetails.ScheduleAppointment}
                          contract={d.overview?.referral?.contractId}
@@ -85,7 +86,7 @@ const Referral: React.FC = () => {
               <p>Select a date for this appointment</p>
               <DayTimePickerField name="appointmentTime" />
             </ChoiceModal>
-    ] }
+    ] : [] }
 
     />
   ;

commit 8f87eb2bddd86d3fe29cffff29f82a802dcc83df

Referrals: fix "Error: Invalid hook call"
Introduced by 1877df70460d28ec1933656d3f599a67a3142627

ui/src/components/MainView.tsx
@@ -114,7 +114,7 @@ const MainView: React.FC = () => {
           <PatientRoutes />
         </Route>
         <Route path="/provider/referrals">
-          <ReferralRoutes />
+          <ReferralRoutes role={username} />
         </Route>
         <Route path="/provider/appointments">
           <AppointmentRoutes />

ui/src/components/Referrals.tsx
@@ -2,16 +2,21 @@ import React, { useState, useMemo } from 'react'
 import { Link, NavLink, Redirect, Route, Switch, useRouteMatch, useParams } from 'react-router-dom';
 import { Main } from '@daml.js/healthcare-claims-processing';
 import { CreateEvent } from '@daml/ledger';
-import { useStreamQuery, useLedger, useParty } from '@daml/react';
+import { useStreamQuery, useLedger } from '@daml/react';
 import { CaretRight, Share } from "phosphor-react";
 import { mapIter, innerJoin, intercalate, Field, FieldsRow, Message, TabLink, useAsync } from "./Common";
 import { Formik, Form, Field as FField, useField } from 'formik';
 import Select from 'react-select';
 import { LField, EField, ChoiceModal, DayTimePickerField, FollowUp, Nothing } from "./ChoiceModal";
 import { TabularScreenRoutes, TabularView, SingleItemView } from "./TabularScreen";
+import { Party } from '@daml/types';
 
-const ReferralRoutes : React.FC = () =>
-  <TabularScreenRoutes metavar=":referralId" table={Referrals} detail={Referral}/>
+type Props = {
+  role: Party;
+}
+
+const ReferralRoutes : React.FC<Props> = ({role}) =>
+  <TabularScreenRoutes metavar=":referralId" table={Referrals} detail={Referral({role})}/>
 
 const useReferrals = (query: any) => {
   const ledger = useLedger();
@@ -52,8 +57,7 @@ const useReferralData = () => {
   return [ { referralId, overview: overviews[0] } ];
 }
 
-const Referral: React.FC = () => {
-  const role = useParty();
+const Referral: React.FC<Props> = ({role}) => {
   return <SingleItemView
     title="Referral"
     useData={useReferralData}