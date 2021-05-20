2b75ce19d81a877ff8fa3c62d72e31b546ebd92a
Rune K. Svendsen <runesvend@gmail.com>
2021-05-20 16:01:16 +0200

    Treatment modal: fix title

--- ui/src/components/Treatments.tsx
+++ ui/src/components/Treatments.tsx
@@ -87,7 +87,7 @@ const Treatment : React.FC = () => {
                    }
       >
               <Message
-                title="Check In Patient"
+                title="Complete Treatment"
                 content={d.overview?.policy?.payload?.patientName + " is present and ready for treatment?"}
               />
             </ChoiceModal>
