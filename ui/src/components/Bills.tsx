0ffc6a8adba352d4245a0d74937d4b8a8fa2182a
ApolloUnicorn <nehomesley@gmail.com>
2021-05-27 19:43:13 -0400

    Drop all caps status

--- ui/src/components/Bills.tsx
+++ ui/src/components/Bills.tsx
@@ -39,7 +39,7 @@ const Bills: React.FC = () => {
              { label: "Provider", getter: o => o.bill?.payload?.provider },
              { label: "Amount", getter: o => o?.bill?.payload?.amount },
              { label: "Procedure Code", getter: o => o?.bill?.payload?.encounterDetails.procedureCode },
-             { label: "Paid", getter: o => (o?.receipt?.payload) ? "YES" : "NO" },
+             { label: "Paid", getter: o => (o?.receipt?.payload) ? "Yes" : "No" },
            ] }
            tableKey={ o => o.bill.contractId }
            itemUrl={ o => o.bill.contractId }
