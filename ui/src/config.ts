0dc1958965ca2c2607b9cc5302c9329426776212
Rune K. Svendsen <runesvend@gmail.com>
2021-05-28 09:11:18 +0200

    fix duplicate license headers

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -3,9 +3,6 @@
 /// SPDX-License-Identifier: Apache-2.0
 ///
 
-// Copyright (c) 2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 export enum DeploymentMode {
   DEV,
   PROD_DABL,
